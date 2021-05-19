7cf53eeab1b5c26d881f713701bb9440f919e095
matthewproskils <matthewthechan@gmail.com>
2021-05-19 13:44:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-67t4x2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-67t4x2)
---
+++ index.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+class App extends Component<AppProps, AppState> {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
